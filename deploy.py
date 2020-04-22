5771b28256356fcb976c5f0c7ded4c5a2a692b2b
LilySu <LilySu@users.noreply.github.com>
2020-04-22 17:18:52 -0400

    changed deploy.py

--- deploy.py
+++ deploy.py
@@ -1,3 +1,57 @@
-from get_data import app
+from flask import Flask
+from flask import jsonify
+import pandas as pd
+import markdown
+import requests
+import os
 
-app.run(host='0.0.0.0', port=80, debug=True)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    with open(os.path.dirname(app.root_path) + '/README.md','r') as markdown_file:
+        content = markdown_file.read()
+        return markdown.markdown(content)
+
+@app.route("/boro")
+def boro():
+    df = pd.read_csv('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/boro.csv')
+    df.fillna(0, inplace=True)
+    return jsonify(df.to_dict('records')),200
+
+@app.route("/by-age")
+def byage():
+    df = pd.read_csv('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-age.csv')
+    df.fillna(0, inplace=True)
+    return jsonify(df.to_dict('records')),200
+
+@app.route("/by-sex")
+def bysex():
+    df = pd.read_csv('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-sex.csv')
+    df.fillna(0, inplace=True)
+    return jsonify(df.to_dict('records')),200
+
+@app.route("/case-hosp-death")
+def casehospdeath():
+    df = pd.read_csv('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/case-hosp-death.csv')
+    df.fillna(0, inplace=True)
+    return jsonify(df.to_dict('records')),200
+
+@app.route("/probable-confirmed-dod")
+def probableconfirmeddod():
+    df = pd.read_csv('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/probable-confirmed-dod.csv')
+    df.fillna(0, inplace=True)
+    return jsonify(df.to_dict('records')),200
+
+@app.route("/summary")
+def summary():
+    df = pd.read_csv('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/summary.csv')
+    df.fillna(0, inplace=True)
+    return jsonify(df.to_dict('records')),200
+
+@app.route("/tests-by-zcta")
+def testsbyzcta():
+    df = pd.read_csv('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/tests-by-zcta.csv')
+    df.fillna(0, inplace=True)
+    return jsonify(df.to_dict('records')),200
